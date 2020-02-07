f073862e15686d7232727a9cdf7279db427643fc
grzesiekbucko@gmail.com <GitBanan4551>
2020-02-07 16:05:50 +0100

    select price

--- src/main/resources/static/js/fragments.js
+++ src/main/resources/static/js/fragments.js
@@ -1,12 +1,21 @@
 
 $(document).ready(function(){
 
-new SlimSelect({
+var select = new SlimSelect({
   select: '#zeg_net_select_brand',
   placeholder: 'Wybierz marki do porównania',
   selectByGroup: true,
   hideSelectedOption: true,
+  closeOnSelect: false,
   searchText: 'Brak wyników',
 })
+
+$("#search_button").click( function()
+           {
+             alert(select.selected());
+           }
+      );
+
+
 
 });
